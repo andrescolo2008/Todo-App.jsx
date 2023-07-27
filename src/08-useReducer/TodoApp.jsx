ebfe7a15541f520128099ac81f8b3a416b9d17ae
Andres Correa <andrescolo2008gmail.com>
2023-07-27 11:18:45 -0500

    Todo-App-borrar y guardar en storage implementado

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./todoReducer"
 import { TodoList } from "./TodoList"
 import { TodoAdd } from "./TodoAdd"
@@ -6,32 +6,40 @@ import { TodoAdd } from "./TodoAdd"
 
 
 const initialState= [
-    {
-        id:new Date().getTime(),
-        description:'Recolectar la piedra del alma ',
-        done:false,
-    },
-    {
-        id:new Date().getTime() * 3,
-        description:'Recolectar la piedra del tiempo ',
-        done:false,
-    }
-
+    // {
+    //     id:new Date().getTime(),
+    //     description:'Recolectar la piedra del alma ',
+    //     done:false,
+    // },
 ]
 
-
+const init= ()=>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState,init)
  
+useEffect(() => {
+localStorage.setItem('todos',JSON.stringify(todos))  
+}, [todos])
+
+
     const handleNewTodo=(todo)=>{
       const action={
         type:'[TODO] Add Todo',
         payload:todo
       }
       dispatch(action);
+    }
+      const handleDeleteTodo=(id)=>{
+        dispatch({
+          type:'[TODO] Remove Todo',
+          payload:id
+        })
+        
     }
  
     return (
@@ -41,19 +49,22 @@ export const TodoApp = () => {
 
     <div className="row">
        <div className="col-7">
-           < TodoList todos={todos} />
+           < TodoList 
+           todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            />
    
        </div>
        <div className="col-5">
         <h4> Agregar TODO</h4>
         <hr />
-             <TodoAdd onNewTodo={handleNewTodo}/>
+             <TodoAdd 
+             onNewTodo={handleNewTodo} />
         </div>
    </div>
 
     </>
   )
 }
-
 
 
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -3,6 +3,8 @@ export const todoReducer=( initialState,action)=>{
     switch(action.type){
         case'[TODO] Add Todo':
 return [...initialState,action.payload] // retornar un nuevo state , bien sea : string, arreglo, booleano, objeto 
+case'[TODO] Remove Todo':
+return initialState.filter(todo=> todo.id !== action.payload)
 
 
         default:
