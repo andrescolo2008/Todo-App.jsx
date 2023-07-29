3547c1aad3a8b73a1f6a3395d96554dde144abb6
Andres Correa <andrescolo2008gmail.com>
2023-07-28 23:16:22 -0500

    TODO-APP-LISTA!

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,51 +1,20 @@
-import { useEffect, useReducer } from "react"
-import { todoReducer } from "./todoReducer"
+
 import { TodoList } from "./TodoList"
 import { TodoAdd } from "./TodoAdd"
+import { useTodo } from "../hooks/useTodo"
+import { TodoItem } from "./TodoItem"
+
 
 const initialState= []
 
-const init= ()=>{
-    return JSON.parse(localStorage.getItem('todos')) || [];
-}
 
 export const TodoApp = () => {
-// crear un componente que se llame useTodos
-// todos,handledeleteTodo,handleNewTodo,handleToggleTodo
-//const [todos,handledeleteTodo,handleNewTodo,handleToggleTodo]=useTodo(), que esto maneje  todo esto  useEffect y todos
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState,init)
- 
-useEffect(() => {
-localStorage.setItem('todos',JSON.stringify(todos))  
-}, [todos])
+const {todos,handleDeleteTodo,handleNewTodo,handleToggleTodo,todosCount,todosPendingCount}=useTodo()
 
-
-    const handleNewTodo=(todo)=>{
-      const action={
-        type:'[TODO] Add Todo',
-        payload:todo
-      }
-      dispatch(action);
-    }
-      const handleDeleteTodo=(id)=>{
-        dispatch({
-          type:'[TODO] Remove Todo',
-          payload:id
-        })
-    }
-
-    const handleToggleTodo=(id)=>{
-
-       dispatch({
-       type:'[TODO] Toggle Todo',
-        payload:id
-       })
-    }
- 
     return (
     <>
-    <h1> TodoApp :2 , <small> pendientes :2 </small></h1>
+    <h1> TodoApp :{todosCount} , <small> pendientes :{ todosPendingCount} </small></h1>
     <hr />
 
     <div className="row">
